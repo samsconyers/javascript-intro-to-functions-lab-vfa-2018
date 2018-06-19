e326126c142fe028818cd23009f6a8960c6ec565
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-19 05:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase ()
 }
 function logShout(string) {
-    console.log(string)
+    console.log(string.toUpperCase())
 }
 describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
