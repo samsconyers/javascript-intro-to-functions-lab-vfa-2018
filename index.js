9bea72d5851160f82b555db24431fe7ef74dd7b2
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-19 05:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ function whisper(string) {
   return string.toLowerCase ()
 }
 
-function logShout(string) {
-  console.log string.toUpperCase
-}
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
 
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
 describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
