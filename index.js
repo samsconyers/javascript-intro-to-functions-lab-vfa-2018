2b346bc510c8ec26e6365eb55637130099c0c4df
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-19 05:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string) {
   return string.toLowerCase ()
 }
 
-describe('logShout(string)', function() {
+function logShout(string) {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
