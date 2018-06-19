3fbf77f4e771798e9092016984819e12c6eed70a
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-19 05:51:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"}
     else if (string.toUpperCase() == string) {
       return "YES INDEED!" }
-      if 
-    }
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+      if (string = "I love you, Grandma.") {
+        return "I love you, too."
+}
